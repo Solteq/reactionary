84fa1599047e045c63839c121ebb95fc2cd931e5
Martin Rogne <martin.rogne@solteq.com>
2025-04-22 21:41:05 +0200

    feat: simple playwright test to ensure browser compat

---
+++ examples/angular/e2e/example.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('displays search results', async ({ page }) => {
+  await page.goto('/');
+
+  await page.waitForSelector('article', { state: 'visible' });
+
+  expect(await page.locator('article').count()).toBe(20);
+});
