de7a5d254027c871e0b52378e1adcd8e994e0e61
Martin Rogne <martin.rogne@solteq.com>
2025-05-15 00:48:50 +0200

    feat: fly.io deployments for vue and angular

---
+++ deployments/angular-trpc/src/router.ts
@@ -0,0 +1,33 @@
+import { initTRPC } from '@trpc/server';
+import {
+    Client,
+  ProductQuerySchema,
+  ProductSchema,
+  SearchIdentifierSchema,
+  SearchResultSchema,
+} from '@reactionary/core';
+
+const t = initTRPC.context<{ client: Client }>().create();
+
+export const router = t.router;
+export const mergeRouters = t.mergeRouters;
+export const publicProcedure = t.procedure;
+
+export const appRouter = router({
+  search: publicProcedure
+    .input(SearchIdentifierSchema)
+    .output(SearchResultSchema)
+    .query(async (opts) => {
+      const result = await opts.ctx.client.search.get(opts.input);
+
+      return result;
+    }),
+  product: publicProcedure
+    .input(ProductQuerySchema)
+    .output(ProductSchema)
+    .query(async (opts) => {
+      return opts.ctx.client.product.get(opts.input);
+    }),
+});
+
+export type RouterType = typeof appRouter;
