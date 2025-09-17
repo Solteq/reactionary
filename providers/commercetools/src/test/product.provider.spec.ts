dc71af537a96eb73ee9b681452b4ca2d154edf74
Asger Jensen <asger.jensen@solteq.com>
2025-09-17 17:41:40 +0200

    test(data): make every test document its test data
    Add testData to top of each file, to make it easier to maintain later

--- providers/commercetools/src/test/product.provider.spec.ts
+++ providers/commercetools/src/test/product.provider.spec.ts
@@ -3,6 +3,14 @@ import { NoOpCache, ProductSchema, Session } from '@reactionary/core';
 import { CommercetoolsProductProvider } from '../providers/product.provider';
 import { createAnonymousTestSession, getCommercetoolsTestConfiguration } from './test-utils';
 
+const testData = {
+  product : {
+    id: '4d28f98d-c446-446e-b59a-d9f718e5b98a',
+    name: 'Sunnai Glass Bowl',
+    image: 'https://storage.googleapis.com/merchant-center-europe/sample-data/goodstore/Sunnai_Glass_Bowl-1.1.jpeg'
+  }
+}
+
 describe('Commercetools Product Provider', () => {
 
     let provider: CommercetoolsProductProvider;
@@ -18,10 +26,10 @@ describe('Commercetools Product Provider', () => {
 
 
   it('should be able to get a product by id', async () => {
-    const result = await provider.getById( { id:  '4d28f98d-c446-446e-b59a-d9f718e5b98a'}, session);
+    const result = await provider.getById( { id: testData.product.id }, session);
 
-    expect(result.identifier.key).toBe('4d28f98d-c446-446e-b59a-d9f718e5b98a');
-    expect(result.name).toBe('Sunnai Glass Bowl');
-    expect(result.image).toBe('https://storage.googleapis.com/merchant-center-europe/sample-data/goodstore/Sunnai_Glass_Bowl-1.1.jpeg');
+    expect(result.identifier.key).toBe(testData.product.id);
+    expect(result.name).toBe(testData.product.name);
+    expect(result.image).toBe(testData.product.image);
   });
 });
