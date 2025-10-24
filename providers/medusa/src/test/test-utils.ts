58978d261872c6124bd1209e747b008840fe231c
Asger Jensen <asger.jensen@solteq.com>
2025-10-24 15:43:50 +0200

    fix(medusa): cart service somewhat functional

--- providers/medusa/src/test/test-utils.ts
+++ providers/medusa/src/test/test-utils.ts
@@ -1,6 +1,8 @@
-import { MedusaConfigurationSchema } from '../schema/configuration.schema.js';
+import 'dotenv/config';
 
-export function getMedusaTestConfiguration() {
+import { MedusaConfigurationSchema, type MedusaConfiguration } from '../schema/configuration.schema.js';
+
+export function getMedusaTestConfiguration(): MedusaConfiguration {
   return MedusaConfigurationSchema.parse({
         publishable_key: process.env['MEDUSA_PUBLISHABLE_KEY'] || '',
         apiUrl: process.env['MEDUSA_API_URL'] || '',
