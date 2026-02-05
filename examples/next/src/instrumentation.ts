14f0b1d8ef18b28e400ccd4ca6e0ccdaa37d5f2e
Martin Rogne <martin.rogne@solteq.com>
2026-02-05 19:57:24 +0100

    feat: upgrade and manage dependencies

--- examples/next/src/instrumentation.ts
+++
@@ -1,9 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    const { NodeSDK } = await import('@opentelemetry/sdk-node');
-
-    const sdk = new NodeSDK();
-    
-    sdk.start();
-  }
-}
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,11 +4,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     projects: ['providers/*', 'examples/*', 'core'],
-    coverage: {
-      provider: 'v8',
-      reporter: ['html'],
-      reportsDirectory: resolve(__dirname) + '/coverage/',
-    },
     reporters: ['default', 'verbose', 'github-actions'],
 
   },
